7ab193f97728f5af32a4d0ebde70ecf63a9cf338
sunnengsen <sunnengsen@gmail.com>
2025-09-04 10:52:06 +0700

    first commit

---
+++ src/test/java/com/wedding/wedding_backend/WeddingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.wedding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
